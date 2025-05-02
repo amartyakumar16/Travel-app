8b32fe259bbf2e554146c3cb23f72e4b70a5d92e
amartyakumar17 <amartyapatel43@gmail.com>
2025-05-03 03:13:32 +0530

    create language section

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,16 +3,18 @@ import { Link } from 'react-router-dom';
 import './Navbar.css';
 
 const Navbar = () => {
-  return (
-    <nav className="navbar">
-      <div className="navbar-logo">
-        <Link to="/">Travel India</Link>
-      </div>
-      <div className="navbar-links">
-        <Link to="/signin" className="signin-button">Sign In</Link>
-      </div>
-    </nav>
-  );
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <Link to="/">Travel India</Link>
+      </div>
+      <div className="navbar-links">
+        <button className="nav-button" onClick={() => alert('Language toggled')}>ENG/HIN</button>
+        <button className="nav-button" onClick={() => alert('Help clicked')}>Help?</button>
+        <Link to="/signin" className="signin-button">Sign In</Link>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
